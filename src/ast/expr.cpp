2b882cd176b64870da35c3bdb7eff19e0a991cdc
ctsk <9384305+ctsk@users.noreply.github.com>
2023-02-22 22:17:57 +0100

    Use namespace

--- src/ast/expr.cpp
+++ src/ast/expr.cpp
@@ -1,25 +1,26 @@
 #include "psql_parse/ast/expr.hpp"
 
-psql_parse::Expression::Expression(psql_parse::location loc)
-: Node { loc } { }
+namespace psql_parse {
+	Expression::Expression(location loc)
+			: Node{loc} {}
 
-psql_parse::NumberLiteral::NumberLiteral(psql_parse::location loc)
-: Expression(loc) { }
+	NumberLiteral::NumberLiteral(location loc)
+			: Expression(loc) {}
 
-psql_parse::IntegerLiteral::IntegerLiteral(location loc, int64_t value)
-: NumberLiteral(loc), value(value) { }
+	IntegerLiteral::IntegerLiteral(location loc, int64_t value)
+			: NumberLiteral(loc), value(value) {}
 
-void psql_parse::IntegerLiteral::negate() {
-	this->value *= -1;
+	void IntegerLiteral::negate() {
+		this->value *= -1;
+	}
+
+	FloatLiteral::FloatLiteral(location loc, double value)
+			: NumberLiteral(loc), value(value) {}
+
+	void FloatLiteral::negate() {
+		this->value *= -1;
+	}
+
+	StringLiteral::StringLiteral(location loc, std::string &&value, StringLiteralType type)
+			: Expression(loc), value(value), type(type) {}
 }
-
-psql_parse::FloatLiteral::FloatLiteral(location loc, double value)
-: NumberLiteral(loc), value(value) { }
-
-void psql_parse::FloatLiteral::negate() {
-	this->value *= -1;
-}
-
-psql_parse::StringLiteral::StringLiteral(location loc, std::string&& value, StringLiteralType type)
-: Expression(loc), value(value), type(type) { }
-
